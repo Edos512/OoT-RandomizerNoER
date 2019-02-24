b91c5b7b0c0086a9d100afa46f921fb52d76c2c2
rattus128 <rattus128@gmail.com>
2019-02-24 20:33:23 +1100

    Go live with entrance rando
    Add the UI element for Entrance Rando to enable.
    
    Currentl master quest is not supported.

--- World.py
+++ World.py
@@ -31,8 +31,6 @@ class World(object):
         self.shop_prices = {}
         self.scrub_prices = {}
         self.light_arrow_location = None
-        #temporary
-        self.shuffle_dungeon_entrances = False
 
         # dump settings directly into world's namespace
         # this gives the world an attribute for every setting listed in Settings.py
