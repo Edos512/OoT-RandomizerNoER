zeldafreakglitcha/OoT-Randomizer

Make can_fill work for non-progression filling.
This is needed for entrance rando where child and adult coupled
entrances will be filled together with one progression and one
not progression.  Make implementation consistent as a signal
logical expression.

Location.py
@@ -43,12 +43,12 @@ class Location(object):
 
 
     def can_fill(self, state, item, check_access=True):
-        if self.minor_only and item.majoritem:
-            return False
         return (
-            not self.is_disabled() and 
+            not (self.minor_only and item.majoritem) and
             self.can_fill_fast(item) and
-            (not check_access or state.can_reach(self)))
+            (not item.isadvancement or
+             (not self.is_disabled() and
+              (not check_access or state.can_reach(self)))))
 
 
     def can_fill_fast(self, item):

Edos512/OoT-RandomizerNoER

Location.py
@@ -47,12 +47,12 @@ class Location(object):
 
 
     def can_fill(self, state, item, check_access=True):
-        if self.minor_only and item.majoritem:
-            return False
         return (
-            not self.is_disabled() and 
+            not (self.minor_only and item.majoritem) and
             self.can_fill_fast(item) and
-            (not check_access or state.can_reach(self)))
+            (not item.advancement or
+             (not self.is_disabled() and
+              (not check_access or state.can_reach(self)))))
 
 
     def can_fill_fast(self, item, manual=False):