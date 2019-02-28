zeldafreakglitcha/OoT-Randomizer

Add optional shorthand age reachability qualifiers to state computations.
This avoid repetitious use of "and can_reach(...)" for the sake of
checking you can reach a location at a particular age.

State.py
@@ -35,6 +35,8 @@ class State(object):
 
 
     def can_reach(self, spot, resolution_hint=None):
+        if (spot == None):
+            return True
         try:
             spot_type = spot.spot_type
             if spot_type == 'Location':
@@ -203,8 +205,9 @@ class State(object):
         return self.has_bombs() or self.has_bombchus()
 
 
-    def can_blast_or_smash(self):
-        return self.has_explosives() or (self.is_adult() and self.has('Hammer'))
+    def can_blast_or_smash(self, adult_reach=None):
+        return self.has_explosives() or \
+               (self.can_reach(adult_reach) and self.is_adult() and self.has('Hammer'))
 
 
     def can_dive(self):
@@ -215,16 +218,19 @@ class State(object):
         return ((self.has('Magic Meter') and self.has('Lens of Truth')) or self.world.logic_lens != 'all')
 
 
-    def has_projectile(self, age='either'):
+    def has_projectile(self, age='either', adult_check=None, child_check=None):
+        as_adult = self.can_reach(adult_check) and \
+            (self.has_explosives() or self.has_bow() or self.has('Progressive Hookshot'))
+        as_child = self.can_reach(child_check) and \
+            (self.has_explosives() or self.has_slingshot() or self.has('Boomerang'))
         if age == 'child':
-            return self.has_explosives() or self.has_slingshot() or self.has('Boomerang')
+            return as_child
         elif age == 'adult':
-            return self.has_explosives() or self.has_bow() or self.has('Progressive Hookshot')
+            return as_adult
         elif age == 'both':
-            return self.has_explosives() or ((self.has_bow() or self.has('Progressive Hookshot')) and (self.has_slingshot() or self.has('Boomerang')))
+            return as_child and as_adult
         else:
-            return self.has_explosives() or ((self.has_bow() or self.has('Progressive Hookshot')) or (self.has_slingshot() or self.has('Boomerang')))
-
+            return as_child or as_adult
 
     def has_GoronTunic(self):
         return (self.has('Goron Tunic') or self.has('Buy Goron Tunic'))
@@ -267,8 +273,9 @@ class State(object):
         )
 
 
-    def has_fire_source(self):
-        return self.can_use('Dins Fire') or self.can_use('Fire Arrows')
+    def has_fire_source(self, adult_reach=None):
+        return self.can_use('Dins Fire') or \
+               self.can_reach(adult_reach) and self.can_use('Fire Arrows')
 
 
     def guarantee_hint(self):

Edos512/OoT-RandomizerNoER

State.py
@@ -138,6 +138,8 @@ class State(object):
 
 
     def has(self, item, count=1):
+        if (item == None):
+            return True;
         return self.prog_items[item] >= count
 
 
@@ -276,8 +278,9 @@ class State(object):
         return self.has_bombs() or self.has_bombchus()
 
 
-    def can_blast_or_smash(self):
-        return self.has_explosives() or (self.is_adult() and self.has('Hammer'))
+    def can_blast_or_smash(self, adult_qualifier=None):
+        return self.has_explosives() or \
+               (self.has(adult_qualifier) and self.is_adult() and self.has('Hammer'))
 
 
     def can_dive(self):
@@ -298,16 +301,19 @@ class State(object):
              self.has('Boomerang') or self.has_explosives() or self.has('Buy_Bottle_Bug'))
 
 
-    def has_projectile(self, age='either'):
+    def has_projectile(self, age='either', adult_qualifier=None, child_qualifier=None):
+        as_adult = self.has(adult_qualifier) and \
+            (self.has_explosives() or self.has_bow() or self.has('Progressive Hookshot'))
+        as_child = self.has(child_qualifier) and \
+            (self.has_explosives() or self.has_slingshot() or self.has('Boomerang'))
         if age == 'child':
-            return self.has_explosives() or self.has_slingshot() or self.has('Boomerang')
+            return as_child
         elif age == 'adult':
-            return self.has_explosives() or self.has_bow() or self.has('Progressive Hookshot')
+            return as_adult
         elif age == 'both':
-            return self.has_explosives() or ((self.has_bow() or self.has('Progressive Hookshot')) and (self.has_slingshot() or self.has('Boomerang')))
+            return as_child and as_adult
         else:
-            return self.has_explosives() or ((self.has_bow() or self.has('Progressive Hookshot')) or (self.has_slingshot() or self.has('Boomerang')))
-
+            return as_child or as_adult
 
     def has_GoronTunic(self):
         return (self.has('Goron Tunic') or self.has('Buy Goron Tunic'))
@@ -355,8 +361,9 @@ class State(object):
         )
 
 
-    def has_fire_source(self):
-        return self.can_use('Dins Fire') or self.can_use('Fire Arrows')
+    def has_fire_source(self, adult_qualifier=None):
+        return self.can_use('Dins Fire') or \
+               self.has(adult_qualifier) and self.can_use('Fire Arrows')
 
 
     def guarantee_hint(self):