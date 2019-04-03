d7340664211e25362f71826e264d616df3a0c168
Roman971 <romanlasnier@hotmail.com>
2019-04-03 19:38:14 +0200

    Rework entrance hints to be generally more useful and more specific

--- Hints.py
+++ Hints.py
@@ -343,23 +343,21 @@ def get_dungeon_hint(spoiler, world, checked):
 
 
 def get_entrance_hint(spoiler, world, checked):
-    hintGroup = getHintGroup('region', world) + getHintGroup('dungeonName', world)
+    entrance_hints = getHintGroup('entrance', world)
+    entrance_hints = list(filter(lambda hint: hint.name not in checked, entrance_hints))
+    valid_entrance_hints = [entrance_hint for entrance_hint in entrance_hints if world.get_entrance(entrance_hint.name).shuffled]
 
-    hint_entrances = []
-    for entrance in world.get_shuffled_entrances():
-        for hint in hintGroup:
-            if entrance.connected_region.name == hint.name or \
-               entrance.connected_region.dungeon and entrance.connected_region.dungeon.name == hint.name:
-                hint_entrances.append(entrance)
-
-    hint_entrances = list(filter(lambda entrance: entrance.name not in checked, hint_entrances))
-    if not hint_entrances:
+    if not valid_entrance_hints:
         return None
 
-    entrance = random.choice(hint_entrances)
+    entrance_hint = random.choice(valid_entrance_hints)
+    entrance = world.get_entrance(entrance_hint.name)
     checked.append(entrance.name)
 
-    area_text = get_hint_area(entrance)
+    entrance_text = entrance_hint.text
+
+    if '#' not in entrance_text:
+        entrance_text = '#%s#' % entrance_text
 
     connected_region = entrance.connected_region
     if connected_region.dungeon:
@@ -370,7 +368,7 @@ def get_entrance_hint(spoiler, world, checked):
     if '#' not in region_text:
         region_text = '#%s#' % region_text
 
-    return (GossipText('%s can be found in #%s#.' % (region_text, area_text), ['Light Blue', 'Green']), None)
+    return (GossipText('%s %s.' % (entrance_text, region_text), ['Light Blue', 'Green']), None)
 
 
 def get_junk_hint(spoiler, world, checked):
